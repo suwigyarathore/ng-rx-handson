4ac1ea8bfc3ea95181bcfd7ebcf8586bd703e3b2
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-19 21:19:57 +0100

    Adding second module

---
+++ src/app/favorite-product/favorite-product.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FavoriteProductComponent } from './favorite-product.component';
+
+@NgModule({
+  declarations: [FavoriteProductComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FavoriteProductModule { }
