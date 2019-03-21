9ddf9d1c087d249d71404166246eedf9b0c12c1a
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-03-21 19:08:35 +0100

    Setting up routes

--- src/app/add-product/add-product.module.ts
+++ src/app/add-product/add-product.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AddProductComponent } from './add-product.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AddProductComponent } from "./add-product.component";
+import { Routes, RouterModule } from "@angular/router";
 
+const routes: Routes = [
+  {
+    path: "",
+    component: AddProductComponent
+  }
+];
 @NgModule({
   declarations: [AddProductComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(routes)]
 })
-export class AddProductModule { }
+export class AddProductModule {}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: "./add-product/add-product.module#AddProductModule"
+  },
+  {
+    path: "cart",
+    loadChildren:
+      "./favorite-product/favorite-product.module#FavoriteProductModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
